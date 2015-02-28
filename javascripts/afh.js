8a6a099f812f7ad3a12a0c22e7b91f698f25e1c2
Andrew Hart <andrew@pogoseat.com>
2015-02-28 09:31:50 -0800

    Add updates to travel, portfolio, and photography pages

---
+++ javascripts/afh.js
@@ -0,0 +1,15 @@
+$(document).ready( function () {
+
+    // Initialize carousels
+    // --------------------------------
+    $('#travel-carousel').owlCarousel({
+
+        autoPlay: 4000,
+         
+        items : 1,
+        itemsDesktop : [1199,3],
+        itemsDesktopSmall : [979,3]
+         
+    });
+
+});
